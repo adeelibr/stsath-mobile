64026497d51b111677d910de679f34b522083ae2
adeelibr <adeelimranr@gmail.com>
2017-01-09 00:19:26 +0500

    Added Login, Splash Screen

---
+++ app/login/LoginPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Container, Header, Title, Button, Icon, Content } from 'native-base';
+
+
+export default class LoginPage extends Component {
+  render() {
+    return (
+      <Container>
+          <Header>
+              <Button transparent>
+                  <Icon name='ios-arrow-back' />
+              </Button>
+              <Title>Header</Title>
+              <Button transparent>
+                  <Icon name='ios-menu' />
+              </Button>
+          </Header>
+
+          <Content>
+              <Text>Yo This Is Looking Good</Text>
+          </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  center: {
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ app/login/SplashPage.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  ActivityIndicator,
+  View,
+  Image
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+import { Style, StyleConstants, Fonts, Images } from '../theme';
+
+const minDuration = 1000;
+const maxDuration = 1500;
+
+export default class Splash extends Component {
+
+  componentDidMount () {
+    let {navigator} = this.props;
+    setTimeout( navigator.push({ id: 2 }) , 2000);
+  }
+
+  randomNumber (min, max) {
+		return Math.random() * max + min;
+	}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.imageStyle}
+          resizeMode={'contain'}
+          source={Images.logo}
+        />
+				<ActivityIndicator
+					animating={true}
+					style={{alignItems: 'center', justifyContent: 'center', top: 125}}
+					color={StyleConstants.secondary}
+					size="large"
+				/>
+	  		<View style={{flexDirection:'row', justifyContent:'space-around', top: -25}} >
+					<Animatable.View // Circle3
+						animation="zoomOut"
+			  		duration={this.randomNumber(minDuration, maxDuration)}
+			  		easing="ease-in"
+            style={styles.Circle3}>
+					</Animatable.View>
+
+          <Animatable.View // Circle1
+						animation="zoomOut"
+			  		duration={this.randomNumber(minDuration, maxDuration)}
+			  		easing="ease-in"
+						style={styles.Circle1}>
+					</Animatable.View>
+
+          <Animatable.View // Circle2
+						animation="zoomOut"
+			  		duration={this.randomNumber(minDuration, maxDuration)}
+			  		easing="ease-in"
+		        style={styles.Circle2}>
+					</Animatable.View>
+        </View>
+			</View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+
+	container: {
+		flex: 1,
+		backgroundColor: StyleConstants.primary,
+		justifyContent: 'center',
+	},
+
+	imageStyle: {
+		top: 75,
+		alignSelf: 'center',
+		width: 260,
+		height: 88,
+		resizeMode: 'contain',
+	},
+
+	Circle1: {
+		opacity: 1,
+		top: -85,
+		left: -50,
+		width: 200,
+		height: 200,
+		borderRadius: 100,
+		backgroundColor: StyleConstants.primary,
+	},
+
+	Circle2: {
+		opacity: 1,
+		width: 150,
+		height: 150,
+		borderRadius: 80,
+		backgroundColor: StyleConstants.primary,
+	},
+
+	Circle3: {
+		opacity: 1,
+		top: -40,
+		left: -50,
+		width: 250,
+		height: 250,
+		borderRadius: 125,
+		backgroundColor: StyleConstants.primary,
+	},
+
+});
