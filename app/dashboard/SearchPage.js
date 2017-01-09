49e6877cd810b1e042f9300f752a378f6080252e
adeelibr <adeelimranr@gmail.com>
2017-01-09 17:51:33 +0500

    Starting to work on SearchPage

--- app/dashboard/SearchPage.js
+++ app/dashboard/SearchPage.js
@@ -21,36 +21,16 @@ export default class SignupPage extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      username: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-    }
+    	word: '',
+    };
   }
 
   componentDidMount () {
-  	AsyncStorage.removeItem('token');
-  }
-
-  goToLoginPage = () => {
-    let {navigator} = this.props;
-    dismissKeyboard();
-    navigator.push({ id: 5 });
+  	// AsyncStorage.removeItem('token');
   }
 
   processForm = () => {
-    let {username, first_name, last_name, email, password, confirmPassword} = this.state;
-    let body = {
-      username,
-      first_name,
-      last_name,
-      email,
-      password,
-      confirmPassword
-    };
-    console.log(body);
+    // Something
   }
 
   render() {
@@ -59,22 +39,18 @@ export default class SignupPage extends Component {
     return (
       <View style={styles.container}>
         <Header>
-          <Button transparent onPress={this.goToLoginPage}>
+          <Button transparent>
             <Icon name='ios-arrow-back' />
           </Button>
-          <Title>Signup</Title>
+          <Title>Smart Twitter Sentiment</Title>
+          <Button transparent>
+            <Icon name='ios-menu' />
+          </Button>
         </Header>
 
         <Content>
 
           <View style={{flex: 1}}>
-
-            <View style={styles.buttonGroup}>
-              <Button style={styles.button} info>
-                Signin
-              </Button>
-            </View>
-
             <TouchableOpacity style={Style.btnDefault} onPress={this.goToLoginPage}>
               <Text style={[Style.f16, styles.text]}>Already Have An Account? Sign In</Text>
             </TouchableOpacity>
@@ -95,27 +71,5 @@ const styles = StyleSheet.create({
     backgroundColor: StyleConstants.secondary,
   },
   
-  form: {
-    alignSelf: 'center', 
-    width: width*0.7, 
-    marginTop: height*0.1, 
-  },
-
-  buttonGroup: {
-    // flex: 1,
-    flexDirection: 'row',
-    alignSelf: 'center', 
-    alignItems: 'center', 
-    marginTop: 20,
-    marginBottom: height*0.1,
-  },
-
-  button: {
-    marginHorizontal: 10,
-  },
-
-  text: {
-    color: StyleConstants.primary,
-  },
 
 });
